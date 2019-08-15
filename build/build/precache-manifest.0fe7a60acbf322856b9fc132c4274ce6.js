841028805aafe104d0ed609a25be820447c9e45e
YUE <1621413443@qq.com>
2019-08-15 22:25:30 +0800

    add react_build

---
+++ build/build/precache-manifest.0fe7a60acbf322856b9fc132c4274ce6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa3eed26e3744a4884217a9c40d7631b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "702d0c86b892368c2b2d",
+    "url": "/static/css/2.6c56fd69.chunk.css"
+  },
+  {
+    "revision": "e10b0510099997858a16",
+    "url": "/static/css/main.dff89b2f.chunk.css"
+  },
+  {
+    "revision": "702d0c86b892368c2b2d",
+    "url": "/static/js/2.686e9e33.chunk.js"
+  },
+  {
+    "revision": "e10b0510099997858a16",
+    "url": "/static/js/main.2f253bc5.chunk.js"
+  },
+  {
+    "revision": "7b6f2350739161096299",
+    "url": "/static/js/runtime~main.f7c36e2a.js"
+  }
+]);
